4a67bad59b6edd32e4c43d46ef65356e777526b0
cpatrick109 <casey.patrick109@gmail.com>
2016-09-01 09:28:51 -0600

    latest blackjack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # q1project
+
+
+//hit cards
+-figure out how to append images to the divs
+-when hand is over clear the divs
+first deal 4 count
+
+var cards=[card1, card2]
+cards[0]
--- app.js
+++ app.js
@@ -1,72 +1,152 @@
-var used_cards = new Array();
+$(document).ready(function() {
+    var deckId;
 
-function card(name, suit, value) {
-    this.name = name;
-    this.suit = suit;
-    this.value = value;
-}
-var deck = [
-    new card('Ace', 'Hearts', 11),
-    new card('Two', 'Hearts', 2),
-    new card('Three', 'Hearts', 3),
-    new card('Four', 'Hearts', 4),
-    new card('Five', 'Hearts', 5),
-    new card('Six', 'Hearts', 6),
-    new card('Seven', 'Hearts', 7),
-    new card('Eight', 'Hearts', 8),
-    new card('Nine', 'Hearts', 9),
-    new card('Ten', 'Hearts', 10),
-    new card('Jack', 'Hearts', 10),
-    new card('Queen', 'Hearts', 10),
-    new card('King', 'Hearts', 10),
-    new card('Ace', 'Diamonds', 11),
-    new card('Two', 'Diamonds', 2),
-    new card('Three', 'Diamonds', 3),
-    new card('Four', 'Diamonds', 4),
-    new card('Five', 'Diamonds', 5),
-    new card('Six', 'Diamonds', 6),
-    new card('Seven', 'Diamonds', 7),
-    new card('Eight', 'Diamonds', 8),
-    new card('Nine', 'Diamonds', 9),
-    new card('Ten', 'Diamonds', 10),
-    new card('Jack', 'Diamonds', 10),
-    new card('Queen', 'Diamonds', 10),
-    new card('King', 'Diamonds', 10),
-    new card('Ace', 'Clubs', 11),
-    new card('Two', 'Clubs', 2),
-    new card('Three', 'Clubs', 3),
-    new card('Four', 'Clubs', 4),
-    new card('Five', 'Clubs', 5),
-    new card('Six', 'Clubs', 6),
-    new card('Seven', 'Clubs', 7),
-    new card('Eight', 'Clubs', 8),
-    new card('Nine', 'Clubs', 9),
-    new card('Ten', 'Clubs', 10),
-    new card('Jack', 'Clubs', 10),
-    new card('Queen', 'Clubs', 10),
-    new card('King', 'Clubs', 10),
-    new card('Ace', 'Spades', 11),
-    new card('Two', 'Spades', 2),
-    new card('Three', 'Spades', 3),
-    new card('Four', 'Spades', 4),
-    new card('Five', 'Spades', 5),
-    new card('Six', 'Spades', 6),
-    new card('Seven', 'Spades', 7),
-    new card('Eight', 'Spades', 8),
-    new card('Nine', 'Spades', 9),
-    new card('Ten', 'Spades', 10),
-    new card('Jack', 'Spades', 10),
-    new card('Queen', 'Spades', 10),
-    new card('King', 'Spades', 10)
-];
+    //define new deck function
+    function getNewDeck() {
+        return $.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1") //(new deck shuffled)
+            // return $.get("http://deckofcardsapi.com/api/deck/new/")(new deck not shuffled)
+    };
+    //define deal 2 cards function
+    function deal2Cards(id) {
+        return $.get(`http://deckofcardsapi.com/api/deck/${id}/draw/?count=2`)
+    }
+    // add to get new deck button event listener
+    $('#newGame').click(function() {
+            getNewDeck().then(function(data) {
+                console.log('data', data);
+                var id = data.deck_id
+                deckId = id;
+                console.log('deckId', deckId);
+            })
 
-var pickCard = function() {
-    var todaysCard = deck[Math.floor(Math.random() * 52)];
-    console.log(todaysCard, Math.floor(Math.random() * 52))
-    return todaysCard;
-};
+        })
+        //add a button to deal cards
+    $('#deal').click(function() {
+        deal2Cards(deckId).then(function(dealData) {
+            console.log('dealData', dealData);
+        })
+    })
 
-document.getElementById("playerCard1").innerHTML = "<img src='images/cards/cards/Spades/Ace.jpg' />";
+
+
+
+    // getCards();
+
+
+
+    // function getCards() {
+    //     $.get("http://deckofcardsapi.com/api/deck/new/").then(function(data) {
+    //         console.log(data);
+    //         var id = data.deck_id
+    //         console.log('id', id);
+    //         return id
+    //     })
+    //
+    // }
+    // var myDeckId = (function() {
+    //     'use strict';
+    //     $.get("http://deckofcardsapi.com/api/deck/new/").then(function(data) {
+    //         var id = data.deck_id
+    //         return id;
+    //     })
+    // }());
+
+    // console.log('myDeckId', myDeckId);
+
+    // getCards().then(function(data) {
+    //     console.log('new data', data);
+    //     let deckID = data.deck_id
+    //     console.log(deckID);
+    //     deal2Cards(deckID).then(function(cardData) {
+    //         console.log(cardData);
+    //     })
+    // })
+
+
+
+
+
+
+});
+
+
+//
+
+
+
+
+
+
+
+// var used_cards = new Array();
+//
+// function card(name, suit, value) {
+//     this.name = name;
+//     this.suit = suit;
+//     this.value = value;
+// }
+// var deck = [
+//     new card('Ace', 'Hearts', 11),
+//     new card('Two', 'Hearts', 2),
+//     new card('Three', 'Hearts', 3),
+//     new card('Four', 'Hearts', 4),
+//     new card('Five', 'Hearts', 5),
+//     new card('Six', 'Hearts', 6),
+//     new card('Seven', 'Hearts', 7),
+//     new card('Eight', 'Hearts', 8),
+//     new card('Nine', 'Hearts', 9),
+//     new card('Ten', 'Hearts', 10),
+//     new card('Jack', 'Hearts', 10),
+//     new card('Queen', 'Hearts', 10),
+//     new card('King', 'Hearts', 10),
+//     new card('Ace', 'Diamonds', 11),
+//     new card('Two', 'Diamonds', 2),
+//     new card('Three', 'Diamonds', 3),
+//     new card('Four', 'Diamonds', 4),
+//     new card('Five', 'Diamonds', 5),
+//     new card('Six', 'Diamonds', 6),
+//     new card('Seven', 'Diamonds', 7),
+//     new card('Eight', 'Diamonds', 8),
+//     new card('Nine', 'Diamonds', 9),
+//     new card('Ten', 'Diamonds', 10),
+//     new card('Jack', 'Diamonds', 10),
+//     new card('Queen', 'Diamonds', 10),
+//     new card('King', 'Diamonds', 10),
+//     new card('Ace', 'Clubs', 11),
+//     new card('Two', 'Clubs', 2),
+//     new card('Three', 'Clubs', 3),
+//     new card('Four', 'Clubs', 4),
+//     new card('Five', 'Clubs', 5),
+//     new card('Six', 'Clubs', 6),
+//     new card('Seven', 'Clubs', 7),
+//     new card('Eight', 'Clubs', 8),
+//     new card('Nine', 'Clubs', 9),
+//     new card('Ten', 'Clubs', 10),
+//     new card('Jack', 'Clubs', 10),
+//     new card('Queen', 'Clubs', 10),
+//     new card('King', 'Clubs', 10),
+//     new card('Ace', 'Spades', 11),
+//     new card('Two', 'Spades', 2),
+//     new card('Three', 'Spades', 3),
+//     new card('Four', 'Spades', 4),
+//     new card('Five', 'Spades', 5),
+//     new card('Six', 'Spades', 6),
+//     new card('Seven', 'Spades', 7),
+//     new card('Eight', 'Spades', 8),
+//     new card('Nine', 'Spades', 9),
+//     new card('Ten', 'Spades', 10),
+//     new card('Jack', 'Spades', 10),
+//     new card('Queen', 'Spades', 10),
+//     new card('King', 'Spades', 10)
+// ];
+//
+// var pickCard = function() {
+//     var todaysCard = deck[Math.floor(Math.random() * 52)];
+//     console.log(todaysCard, Math.floor(Math.random() * 52))
+//     return todaysCard;
+// };
+//
+// document.getElementById("playerCard1").innerHTML = "<img src='images/cards/cards/Spades/Ace.jpg' />";
 // console.log(pickCard())
 
 // var hand = {
